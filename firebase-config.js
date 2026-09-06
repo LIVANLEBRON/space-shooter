b93f41396c5b7aaf06c1831cec4c3e327e0ac52e
LIVAN ANDRES LEBRON URBAEZ <199433925+LIVANLEBRON@users.noreply.github.com>
2026-09-06 19:29:18 -0400

    Update Firebase project configuration to space-shooter-ranking-2026 and include finalBossProgress rules

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Configuración pública de Firebase. El juego usa las API REST de Auth y Firestore
 // para conservar HTML, CSS y JavaScript puro sin depender de módulos externos.
 window.VOID_FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyDDzVjGpfWnk52N2K4xaDIrnQuW9nKjC1k',
-  authDomain: 'com-example-myapplicatio-32160.firebaseapp.com',
-  projectId: 'com-example-myapplicatio-32160',
-  storageBucket: 'com-example-myapplicatio-32160.firebasestorage.app',
-  messagingSenderId: '394959515213',
-  appId: '1:394959515213:web:31a144708b4d0c1a25b604'
+  apiKey: 'AIzaSyCCBJLRimq-IAayRW9Yu8RKNE4YyIM4XvE',
+  authDomain: 'space-shooter-ranking-2026.firebaseapp.com',
+  projectId: 'space-shooter-ranking-2026',
+  storageBucket: 'space-shooter-ranking-2026.firebasestorage.app',
+  messagingSenderId: '545255879448',
+  appId: '1:545255879448:web:3b691e3bf06963ebfc246d'
 };
